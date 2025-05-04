99c6fd776d531f0af837954d12b2bfa68fde326a
4xxxxdyy <2130434093@qq.com>
2025-05-04 16:16:11 +0800

    a

---
+++ my-next-app/components/Hello.js
@@ -0,0 +1,7 @@
+export default function hello() {
+    return (
+        <div>
+            <img src="https://i-blog.csdnimg.cn/blog_migrate/f130cfa4f1baf37340055d02dab9a40c.png" alt="" />
+         </div>
+    )
+}
