ada641c85c1bbdcd11d50dc90e16ac8bcfb17a37
4xxxxdyy <2130434093@qq.com>
2025-06-22 19:10:38 +0800

    s

--- my-dev-portal/app/index.tsx
+++ my-dev-portal/app/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
-import ExerciseCard from '@/components/ExerciseCard'
+import ExerciseCard from '@/app/components/ExerciseCard'
 
 // ⚠️ 替换为你的真实练习数据
 const exercises = [
